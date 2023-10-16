5bd53a613aa83a1176148857e5c8d250df03caed
Panos Malliotas <panagiotis.malliotas@datawise.ai>
2023-10-16 17:20:59 +0300

    test

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,9 +2,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import packageJson from "./package.json" assert { type: "json" };
 import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
+import packageJson from "./package.json" assert { type: "json" }
 
 export default [
   {
@@ -13,24 +14,38 @@ export default [
       {
         file: packageJson.main,
         format: "cjs",
+        sourcemap: true,
       },
       {
         file: packageJson.module,
         format: "esm",
+        sourcemap: true,
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      terser()
-
+      terser(),
+    ],
+    external: [
+      "react",
+      "react-dom",
+      "react-hook-form",
+      "@mantine/core",
+      "@mantine/hooks",
+      "@mantine/dates",
+      "autoprefixer",
+      "dayjs",
+      "postcss",
+      "tailwindcss",
+      "zod"
     ],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
+    input: "src/index.ts",
+    output: [{ file: "dist/types.d.ts", format: "es" }],
+    plugins: [dts.default()],
   },
-
 ];
