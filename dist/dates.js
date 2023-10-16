6dfe9bb7dfad43ba8cb482ef7fb137cae8622e17
Panos Malliotas <panagiotis.malliotas@datawise.ai>
2023-10-16 15:42:11 +0300

    changes

--- dist/dates.js
+++ dist/dates.js
@@ -1,2 +1,2 @@
 import{ak as r,al as t}from"./Modal-rs0JaEQT.js";import{C as s,D as n,M as o,Y as i}from"./YearPickerInput-Dg4CX5K5.js";export{b as DateInput,c as DatePickerInput,a as DatesProvider,d as MonthPickerInput,T as TimeInput,e as YearPickerInput}from"./YearPickerInput-Dg4CX5K5.js";import"react";import"react-dom";import"./use-disclosure-m6NeHj15.js";import"dayjs";function u(a){return r.jsx(s,t({},a))}function p(a){return r.jsx(n,t({},a))}function m(a){return r.jsx(o,t({},a))}function j(a){return r.jsx(i,t({},a))}export{u as Calendar,p as DateTimePicker,m as MonthPicker,j as YearPicker};
-//# sourceMappingURL=index2.js.map
+//# sourceMappingURL=dates.js.map
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,15 +6,18 @@ import { terser } from "rollup-plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import babel from "rollup-plugin-babel"
 
-import packageJson from "./package.json" assert { type: "json" }
-
 export default [
   {
-    input: ["src/components/core/index.ts", "src/components/dates/index.ts", "src/components/forms/index.ts", "src/hooks/index.ts"],
+    input: {
+      core: "src/components/core/index.ts",
+      dates: "src/components/dates/index.ts",
+      forms: "src/components/forms/index.ts",
+      hooks: "src/hooks/index.ts"
+    },
     output:
     {
       dir: "dist",
-      format: "esm",
+      format: "es",
       sourcemap: true,
     },
     plugins: [
@@ -23,15 +26,15 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json", declarationDir: "dist" }),
       terser(),
-      babel({
-        exclude: 'node_modules/**', // Exclude node_modules
-        babelHelpers: 'runtime',
-        presets: [
-          ['@babel/preset-env', { targets: 'last 2 versions, > 1%' }],
-          '@babel/preset-react',
-        ],
-        plugins: ['@babel/plugin-transform-runtime'],
-      }),
+      // babel({
+      // exclude: 'node_modules/**', // Exclude node_modules
+      // babelHelpers: 'runtime',
+      // presets: [
+      //   ['@babel/preset-env', { targets: 'last 2 versions, > 1%' }],
+      //   '@babel/preset-react',
+      // ],
+      // plugins: ['@babel/plugin-transform-runtime'],
+      // }),
     ],
     external: [
       "react",
