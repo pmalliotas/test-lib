f9b72580a4b9e7519de4d48d442a1fd8237a83f4
Panos Malliotas <panagiotis.malliotas@datawise.ai>
2023-10-16 17:32:44 +0300

    change

--- dist/esm/types/components/core/Misc/Transition/Transition.d.ts
+++
@@ -1,3 +0,0 @@
-import { type TransitionProps as MantineTransitionProps } from "@mantine/core";
-export type TransitionProps = MantineTransitionProps & {};
-export declare function Transition(props: TransitionProps): import("react/jsx-runtime").JSX.Element;
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,51 +1,85 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+// rollup.config.js
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
+import image from '@rollup/plugin-image';
+import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
+import del from 'rollup-plugin-delete';
 
-import packageJson from "./package.json" assert { type: "json" }
+// Check if we're in development mode
+const isDev = process.env.NODE_ENV !== 'production';
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
-    ],
-    external: [
-      "react",
-      "react-dom",
-      "react-hook-form",
-      "@mantine/core",
-      "@mantine/hooks",
-      "@mantine/dates",
-      "autoprefixer",
-      "dayjs",
-      "postcss",
-      "tailwindcss",
-      "zod"
-    ],
-  },
-  {
-    input: "src/index.ts",
-    output: [{ file: "dist/types.d.ts", format: "es" }],
-    plugins: [dts.default()],
-  },
-];
+export default {
+  input: 'src/index.ts', // our source file
+  output: [
+    {
+      dir: 'dist', // the directory for output files
+      format: 'esm', // suitable format for tree-shaking
+      sourcemap: true, // sourcemaps should be output as separate files
+    },
+  ],
+  // Apply plugins
+  plugins: [
+    // Delete the previous build files
+    del({ targets: 'dist/*' }),
+
+    // Preferably use this at the start of your plugins list to automatically exclude peer dependencies from your bundle
+    peerDepsExternal(),
+
+    // Locate modules using the Node resolution algorithm, for using third-party modules in node_modules
+    resolve(),
+
+    // Convert CommonJS modules to ES6, so they can be included in a Rollup bundle
+    commonjs(),
+
+    // Handle json files
+    json(),
+
+    // Handle image files
+    image(),
+
+    // Compile TypeScript files
+    typescript({
+      tsconfig: "./tsconfig.json",
+      declarationDir: "dist"
+    }),
+
+    // Handle .css, .scss, .less files
+    // postcss({
+    //   extract: false,
+    //   modules: true,
+    //   use: ['sass'],
+    // }),
+
+    // Replace strings in files while bundling
+    // replace({
+    //   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    //   preventAssignment: true,
+    // }),
+
+    // Minify the bundle
+    terser(),
+
+    // Serve and live reload in development mode
+    ...(isDev
+      ? [
+        serve({
+          open: true,
+          contentBase: 'dist',
+          historyApiFallback: true,
+          host: 'localhost',
+          port: 3000,
+        }),
+        livereload({ watch: 'dist' }),
+      ]
+      : []),
+  ],
+  // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment', etc.)
+  external: [/* 'react', 'react-dom' */],
+};
