8f090f3df12c93951cefa942bdbe2b1c97c923a8
Panos Malliotas <panagiotis.malliotas@datawise.ai>
2023-10-20 16:11:06 +0300

    test

--- src/core/Buttons/ActionIcon/ActionIcon.tsx
+++ src/core/Buttons/ActionIcon/ActionIcon.tsx
@@ -2,11 +2,18 @@ import { ActionIcon as MantineActionIcon, createPolymorphicComponent } from "@ma
 import { type ActionIconProps as MantineActionIconProps } from "@mantine/core"
 import { forwardRef } from "react"
 
+import classes from "./ActionIcon.module.css"
+
 export type ActionIconProps = MantineActionIconProps & {}
 
 export const ActionIcon: React.FC<ActionIconProps> = createPolymorphicComponent<"button", ActionIconProps>(forwardRef<HTMLButtonElement, ActionIconProps>((props, ref) => {
     return (
-        <MantineActionIcon {...props} ref={ref} />
+        <MantineActionIcon
+            classNames={{
+                loader: classes.loader
+            }}
+            {...props} ref={ref}
+        />
     )
 }))
 
