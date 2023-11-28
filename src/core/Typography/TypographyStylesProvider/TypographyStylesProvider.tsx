e354de321dfdf0ff3ac46bfc7282a8e03cd9cf42
Panos Malliotas <panagiotis.malliotas@datawise.ai>
2023-11-28 19:34:13 +0200

    add react

--- src/core/Typography/TypographyStylesProvider/TypographyStylesProvider.tsx
+++ src/core/Typography/TypographyStylesProvider/TypographyStylesProvider.tsx
@@ -1,3 +1,5 @@
+import * as React from "react"
+
 import { TypographyStylesProvider as MantineTypographyStylesProvider } from "@mantine/core"
 import { type TypographyStylesProviderProps as MantineTypographyStylesProviderProps } from "@mantine/core"
 
